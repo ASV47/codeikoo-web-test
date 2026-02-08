4d5d6b624ebd9c3be0fda7c8c963549924c8c23f
ASV47 <ahmedmohammedmahermakled@gmail.com>
2026-02-08 02:42:36 +0200

    tryback

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://codeikoo-backend-dotnet-prod.eba-mqskcvmc.eu-central-1.elasticbeanstalk.com'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://codeikoo-backend-dotnet-prod.eba-mqskcvmc.eu-central-1.elasticbeanstalk.com'
+};
